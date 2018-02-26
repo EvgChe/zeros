8c72b5e55fe3d8f4dcec08001da27ffe3458b24c
EvgChe <chetkova@list.ru>
2018-02-26 12:16:16 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    var value = 0;
+    var zero = 0;
+    var a;
+
+    while ( value < number ) {
+
+        value++;
+        a = value;
+
+        while ( a % 5 == 0 ) {
+
+            zero ++;
+            a = a / 5;
+
+        }
+
+    }
+
+    return zero;
+
 }
+
+//console.log(getZerosCount(26));
